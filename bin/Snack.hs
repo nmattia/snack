cb521b9776d25c871f54cb6394526b1347624cca
Nicolas Mattia <nicolas@nmattia.com>
2018-07-06 13:46:26 +0200

    Snack.hs: simplify nix incantations

--- bin/Snack.hs
+++ bin/Snack.hs
@@ -9,15 +9,16 @@ import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Aeson ((.:))
-import Data.ByteString as BS
 import Data.FileEmbed (embedStringFile)
-import Data.Semigroup
+import Data.List (intercalate)
+import Data.Semigroup ((<>))
 import Data.String (fromString)
 import Data.String.Interpolate
-import Data.Text as T
 import Shelly (Sh)
 import System.Posix.Process (executeFile)
 import qualified Data.Aeson as Aeson
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
 import qualified Options.Applicative as Opts
 import qualified Shelly as S
 
@@ -73,9 +74,6 @@ instance Aeson.FromJSON Project where
 
 parseMode :: Opts.Parser Mode
 parseMode =
-    -- Opts.flag Standalone Standalone
-      -- (Opts.long "standalone")
-
     ((Standalone . mkSnackNix) <$>
         Opts.strOption
         (Opts.long "snack-nix"
@@ -84,13 +82,11 @@ parseMode =
         <> Opts.metavar "PATH")
         )
     <|>
-    -- Opts.flag HPack HPack
-      -- (Opts.long "hpack")
-
     ((HPack . mkPackageYaml) <$>
         Opts.strOption
         (Opts.long "package-yaml"
         <> Opts.value "./package.yaml"
+        <> Opts.short 'p'
         <> Opts.metavar "PATH")
         )
 
@@ -102,11 +98,25 @@ snackGhci snackNix = do
     path <- S.print_stdout False $ exePath <$> snackBuildGhci snackNix
     S.print_stdout True $ run_ (fromString path) []
 
-snackBuildGhci :: SnackNix -> Sh Project
-snackBuildGhci snackNix = do
-    out <- runStdin1
+data NixArg = NixArg
+  { argType :: NixArgType
+  , argName :: T.Text
+  , argValue :: T.Text
+  }
+
+data NixArgType
+  = ArgStr
+  | Arg
+
+newtype NixExpr = NixExpr { unNixExpr :: T.Text }
+
+newtype NixPath = NixPath { unNixPath :: T.Text }
+
+snackBuildWith :: [NixArg] -> NixExpr -> Sh NixPath
+snackBuildWith extraNixArgs nixExpr =
+    NixPath <$> runStdin1
       (T.pack [i|
-        { snackNix, lib64, specJson }:
+        { #{ intercalate "," funArgs } }:
         let
           spec = builtins.fromJSON specJson;
           pkgs = import (builtins.fetchTarball
@@ -125,105 +135,68 @@ snackBuildGhci snackNix = do
                 chmod +w $out
               '';
           snack = pkgs.callPackage libDir {};
-          proj = snack.executable (import snackNix);
-        in
-          { build = proj.build;
-            ghci = proj.ghci;
-          }
-      |]
-      )
+        in #{ T.unpack $ unNixExpr $ nixExpr }
+      |])
       "nix-build"
-      [ "-"
-      , "--arg", "snackNix", T.pack $ unSnackNix snackNix
-      , "--argstr", "lib64", libb64
-      , "--argstr", "specJson", specJson
-      , "--no-out-link"
-      , "-A", "ghci.json"
+      cliArgs
+  where
+    cliArgs :: [T.Text]
+    cliArgs =
+      [ "-" -- read expression from stdin
+      , "--no-out-link" -- no need for roots
+      ] <> (concatMap toCliArgs nixArgs)
+    funArgs :: [String]
+    funArgs = toFunArg <$> nixArgs
+    nixArgs :: [NixArg]
+    nixArgs =
+      [ NixArg { argType = ArgStr , argName = "specJson", argValue = specJson }
+      , NixArg { argType = ArgStr , argName = "lib64", argValue = libb64 }
+      ] <> extraNixArgs
+    toFunArg :: NixArg -> String
+    toFunArg = T.unpack . argName
+    toCliArgs :: NixArg -> [T.Text]
+    toCliArgs narg = case argType narg of
+      { Arg -> "--arg"; ArgStr -> "--argstr" }
+      : [ argName narg , argValue narg ]
+
+snackBuild :: SnackNix -> Sh Project
+snackBuild snackNix = do
+    NixPath out <- snackBuildWith
+      [ NixArg
+          { argName = "snackNix"
+          , argValue = T.pack $ unSnackNix snackNix
+          , argType = Arg
+          }
       ]
+      $ NixExpr "(snack.executable (import snackNix)).build.json"
+    json <- liftIO $ BS.readFile (T.unpack out)
+    let Just proj = Aeson.decodeStrict' json
+    pure proj
+
+snackBuildGhci :: SnackNix -> Sh Project
+snackBuildGhci snackNix = do
+    NixPath out <- snackBuildWith
+      [ NixArg
+          { argName = "snackNix"
+          , argValue = T.pack $ unSnackNix snackNix
+          , argType = Arg
+          }
+      ]
+      $ NixExpr "(snack.executable (import snackNix)).ghci.json"
     json <- liftIO $ BS.readFile (T.unpack out)
     let Just proj = Aeson.decodeStrict' json
     pure proj
 
 snackBuildHPack :: PackageYaml -> Sh Project
 snackBuildHPack packageYaml = do
-    out <- runStdin1
-      (T.pack [i|
-        { packageYaml, lib64, specJson }:
-        let
-          spec = builtins.fromJSON specJson;
-          pkgs = import (builtins.fetchTarball
-            { url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
-              sha256 = spec.sha256;
-            }) {} ;
-          libDir =
-            let
-              b64 = pkgs.writeTextFile { name = "lib-b64"; text = lib64; };
-            in
-              pkgs.runCommand "snack-lib" {}
-              ''
-                cat ${b64} | base64 --decode > out.tar.gz
-                mkdir -p $out
-                tar -C $out -xzf out.tar.gz
-                chmod +w $out
-              '';
-          snack = pkgs.callPackage libDir {};
-          proj = snack.packageYaml packageYaml;
-        in
-          { build = proj.library.build;
-            ghci = proj.librayr.build;
+    NixPath out <- snackBuildWith
+      [ NixArg
+          { argName = "packageYaml"
+          , argValue = T.pack $ unPackageYaml packageYaml
+          , argType = Arg
           }
-      |]
-      )
-      "nix-build"
-      [ "-"
-      , "--arg", "packageYaml", T.pack $ unPackageYaml packageYaml
-      , "--argstr", "lib64", libb64
-      , "--argstr", "specJson", specJson
-      , "--no-out-link"
-      , "-A", "build.json"
       ]
-    json <- liftIO $ BS.readFile (T.unpack out)
-    let Just proj = Aeson.decodeStrict' json
-    pure proj
-
-snackBuild :: SnackNix -> Sh Project
-snackBuild snackNix = do
-    out <- runStdin1
-      (T.pack [i|
-        { snackNix, lib64, specJson }:
-        let
-          spec = builtins.fromJSON specJson;
-          pkgs = import (builtins.fetchTarball
-            { url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
-              sha256 = spec.sha256;
-            }) {} ;
-          libDir =
-            let
-              b64 = pkgs.writeTextFile { name = "lib-b64"; text = lib64; };
-            in
-              pkgs.runCommand "snack-lib" {}
-              ''
-                cat ${b64} | base64 --decode > out.tar.gz
-                mkdir -p $out
-                tar -C $out -xzf out.tar.gz
-                chmod +w $out
-              '';
-          snack = pkgs.callPackage libDir {};
-          proj = snack.executable (import snackNix);
-        in
-          { build = proj.build;
-            ghci = proj.ghci;
-          }
-      |]
-      )
-      "nix-build"
-      [ "-"
-      , "--arg", "snackNix", T.pack $ unSnackNix snackNix
-      , "--argstr", "lib64", libb64
-      , "--argstr", "specJson", specJson
-      , "--no-out-link"
-      , "-A", "build.json"
-      ]
+      $ NixExpr "(snack.packageYaml packageYaml).library.build.json"
     json <- liftIO $ BS.readFile (T.unpack out)
     let Just proj = Aeson.decodeStrict' json
     pure proj
