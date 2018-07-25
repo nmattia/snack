13f6ce1d31ba8554bc9092115a3ded01eabf8b0f
Nicolas Mattia <nicolas@nmattia.com>
2018-07-25 22:20:40 +0200

    Support older nix version

--- nix/nixpkgs/default.nix
+++ nix/nixpkgs/default.nix
@@ -1,6 +1,17 @@
 let
   spec = builtins.fromJSON (builtins.readFile ./nixpkgs-src.json);
   rev = spec.rev;
+  sha256 = spec.sha256;
   url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
+
+  # fetchTarball version that is compatible between all the versions of Nix
+  fetch = { url, sha256 }@attrs:
+    let
+      inherit (builtins) lessThan nixVersion fetchTarball;
+    in
+    if lessThan nixVersion "1.12" then
+      fetchTarball { inherit url; }
+    else
+      fetchTarball attrs;
 in
-  builtins.fetchTarball url
+  fetch { inherit url sha256; }
