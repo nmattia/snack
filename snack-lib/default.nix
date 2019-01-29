ecefcc15dd9b0c5e60a80c524adfc9144538456e
Nicolas Mattia <nicolas@nmattia.com>
2019-01-29 22:15:09 +0100

    Major build interface refactor

--- snack-lib/default.nix
+++ snack-lib/default.nix
@@ -19,24 +19,106 @@ with (callPackage ./package-spec.nix {});
 with (callPackage ./hpack.nix {});
 
 let
+
+  # Derivation that creates a binary in a 'bin' folder.
+  executable = packageFile:
+    let
+      specs = specsFromPackageFile packageFile;
+      spec =
+        if pkgs.lib.length specs == 1
+        then pkgs.lib.head specs
+        else abort "'executable' can only be called on a single executable";
+      exe =
+        if spec.packageIsExe
+        then buildAsExecutable spec
+        else abort "'executable' called on a library";
+    in exe.out;
+
+
+  # Build a package spec as resp. a library and an executable
+
+  buildAsLibrary = pkgSpec:
+    buildLibrary ghcWith (libraryModSpecs pkgSpec);
+
+  buildAsExecutable = pkgSpec:
+    let drv = linkMainModule ghcWith (executableMainModSpec pkgSpec);
+    in
+      { out = drv.out;
+        exe_path = "${drv.out}/${drv.relExePath}";
+      };
+
   ghcWith = deps: ghcWithPackages
     (ps: map (p: ps.${p}) deps);
 
-  # Assumes the package description describes an executable
-  withMainModSpec = pkgDescr: act:
+  specsFromPackageFile = packageFile:
     let
-      mainModName = pkgDescr.packageMain;
-      mainModSpec =
+      basename = builtins.baseNameOf packageFile;
+      components = pkgs.lib.strings.splitString "." basename;
+      ext =
+        if pkgs.lib.length components <= 1
+        then abort ("File " ++ packageFile ++ " does not have an extension")
+        else pkgs.lib.last components;
+      fromNix = [(mkPackageSpec (import packageFile))];
+      fromHPack =
         let
-          fld = moduleSpecFold' modSpecs;
-          modSpecs = foldDAG fld [mainModName];
-        in modSpecs.${mainModName};
-      drv = linkMainModule ghcWith mainModSpec;
-    in
-      { out = drv.out;
-        outPath = "${drv.out}";
-        exePath = "${drv.out}/${drv.relExePath}";
-      };
+          descrs = pkgDescrsFromHPack packageFile;
+          executables =
+            builtins.map mkPackageSpec descrs.executables;
+          library = withAttr descrs "library" null
+              (comp: if builtins.isNull comp then null else mkPackageSpec comp);
+        in executables ++ (if builtins.isNull library then [] else [ library ]);
+      specs =
+        if ext == "nix" then fromNix
+        else if ext == "yaml" then fromHPack
+        else if ext == "yml" then fromHPack
+        else abort ("Unknown extension " ++ ext ++ " of file " ++ packageFile);
+    in specs;
+
+  # Normal build (libs, exes)
+
+  inferBuild = packageFile:
+    mkPackages (specsFromPackageFile packageFile);
+
+  mkPackages = pkgSpecs: writeText "build.json"
+    ( builtins.toJSON
+      ( builtins.map
+          (pkgSpec:
+            if pkgSpec.packageIsExe
+            then
+              { build_type = "executable";
+                result = buildAsExecutable pkgSpec;
+              }
+            else
+              { build_type = "library";
+                result = buildAsLibrary pkgSpec;
+              }
+          ) pkgSpecs
+      )
+    );
+
+  # GHCi build (libs, exes)
+
+  inferGhci = packageFile:
+    mkPackagesGhci (specsFromPackageFile packageFile);
+
+  mkPackagesGhci = pkgSpecs: writeText "hpack-ghci-json"
+    ( builtins.toJSON (
+        builtins.map
+          (pkgSpec:
+            let
+              drv =
+                if pkgSpec.packageIsExe
+                then ghciWithMain ghcWith (executableMainModSpec pkgSpec)
+                else ghciWithModules ghcWith (libraryModSpecs pkgSpec)
+                ;
+            in
+            { build_type = "ghci"; # TODO: need to record the name somewhere
+              result = "${drv.out}/bin/ghci-with-files";
+              }
+          ) pkgSpecs
+    ));
+
+  # How to build resp. libraries and executables
 
   libraryModSpecs = pkgSpec:
     let
@@ -58,143 +140,13 @@ let
         in modSpecs.${mainModName};
     in mainModSpec;
 
-  buildAsLibrary = pkgSpec:
-    buildLibrary ghcWith (libraryModSpecs pkgSpec);
-
-  buildAsExecutable = pkgSpec:
-    let drv = linkMainModule ghcWith (executableMainModSpec pkgSpec);
-    in
-      { out = drv.out;
-        exe_path = "${drv.out}/${drv.relExePath}";
-      };
-
-  # TODO: deduplicate extensions + update README with --package-file
-  inferBuild = packageFile:
-    let
-      basename = builtins.baseNameOf packageFile;
-      components = pkgs.lib.strings.splitString "." basename;
-      ext =
-        if pkgs.lib.length components <= 1
-        then abort ("File " ++ packageFile ++ " does not have an extension")
-        else pkgs.lib.last components;
-      build =
-        if ext == "nix" then inferSnackBuild
-        else if ext == "yaml" then inferHPackBuild
-        else if ext == "yml" then inferHPackBuild
-        else abort ("Unknown extension " ++ ext ++ " of file " ++ packageFile);
-    in build packageFile;
-
-  inferGhci = packageFile:
-    let
-      basename = builtins.baseNameOf packageFile;
-      components = pkgs.lib.strings.splitString "." basename;
-      ext =
-        if pkgs.lib.length components <= 1
-        then abort ("File " ++ packageFile ++ " does not have an extension")
-        else pkgs.lib.last components;
-      ghci =
-        if ext == "nix" then inferSnackGhci
-        else if ext == "yaml" then inferHPackGhci
-        else if ext == "yml" then inferHPackGhci
-        else abort ("Unknown extension " ++ ext ++ " of file " ++ packageFile);
-    in ghci packageFile;
-
-  inferSnackBuild = packageNix: mkPackage (import packageNix);
-
-  inferSnackGhci = packageNix: writeText "snack-ghci-json"
-    ( builtins.toJSON (
-    let
-      pkgSpec = mkPackageSpec (import packageNix);
-      drv =
-        if builtins.isNull pkgSpec.packageMain
-        then ghciWithModules ghcWith (libraryModSpecs pkgSpec)
-        else ghciWithMain ghcWith (executableMainModSpec pkgSpec);
-    in
-      { build_type = "ghci";
-        result = {
-            "ghci_path" = "${drv.out}/bin/ghci-with-files";
-          };
-      }
-    ));
-
-  inferHPackBuild = packageYaml: writeText "hpack-build-json"
-    ( builtins.toJSON (
-    let pkgSpecs = hpackSpecs packageYaml;
-    in
-      { build_type = "multi";
-        result =
-          { library =
-              if builtins.isNull pkgSpecs.library
-              then null
-              else buildAsLibrary (pkgSpecs.library);
-            executables = lib.attrsets.mapAttrs (k: v: buildAsExecutable v) pkgSpecs.executables;
-          };
-      }
-    ));
-
-  inferHPackGhci = packageYaml: writeText "hpack-ghci-json"
-    ( builtins.toJSON (
-    let
-      pkgSpecs = hpackSpecs packageYaml;
-      pkgSpec = mkPackageSpec (import packageNix);
-      drv =
-        let exeSpecs = builtins.attrValues pkgSpecs.executables;
-        in
-          if lib.lists.length exeSpecs == 1
-          then ghciWithMain ghcWith (executableMainModSpec (lib.lists.head exeSpecs))
-          else
-            if builtins.isNull pkgSpecs.library
-            then abort "GHCi: needs either a single executable or a library"
-            else ghciWithModules ghcWith (libraryModSpecs pkgSpecs.library);
-    in
-      { build_type = "ghci";
-        result = {
-            "ghci_path" = "${drv.out}/bin/ghci-with-files";
-          };
-      }
-    ));
-
-  snackSpec = packageNix: mkPackageSpec (import packageNix);
-  hpackSpecs = packageYaml:
-    let
-      descrs = pkgDescrsFromHPack packageYaml;
-    in
-      { library = withAttr descrs "library" null
-          (comp: if builtins.isNull comp then null else mkPackageSpec comp);
-        executables =
-          lib.attrsets.mapAttrs (k: v: mkPackageSpec v) descrs.executables;
-      };
-
-  mkPackage = snackNixExpr: writeText "snack-build-json"
-    ( builtins.toJSON (
-    let
-      pkgSpec = mkPackageSpec snackNixExpr;
-    in
-      if builtins.isNull pkgSpec.packageMain
-      then
-        { "build_type" = "library";
-          "result" = buildAsLibrary pkgSpec;
-        }
-      else
-        { "build_type" = "executable";
-          "result" = buildAsExecutable pkgSpec;
-        }
-    ));
-
 in
   {
     inherit
     inferBuild
     inferGhci
-    inferSnackBuild
-    inferSnackGhci
-    inferHPackBuild
-    inferHPackGhci
-    packageYaml
     buildAsExecutable
     buildAsLibrary
-    snackSpec
-    hpackSpecs
-    mkPackage
+    executable
     ;
   }
